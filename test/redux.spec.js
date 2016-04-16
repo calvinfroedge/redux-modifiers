0fdf12b5ded48262bb10b8564a02c4d15963a85d
Calvin Froedge <calvinfroedge@gmail.com>
2016-04-16 19:47:40 -0400

    Add tests w/ objects and targeting on example reducer

--- test/redux.spec.js
+++ test/redux.spec.js
@@ -1,24 +1,35 @@
 import expect from 'expect'
-import { array, target } from '../src'
+import { array, object, target } from '../src'
 import { createAction, handleActions } from 'redux-actions'
 import { createStore } from 'redux'
 
 let add = createAction('add');
+let update = createAction('update');
 
 const reducer = handleActions({
   'add': array.add
 }, []);
 
-describe('Test object modifiers', ()=>{
- 
+const objectReducer = handleActions({
+  'update': target('foo', object.update)
+}, {foo: { bar: 'baz' } });
+
+describe('Test with actual reducer', ()=>{
   var store;
+  var store2;
 
   before(()=>{
     store = createStore(reducer);
+    store2 = createStore(objectReducer);
   });
 
   it('Should add item to the reducer', ()=>{
     store.dispatch(add('foo'));
     expect(store.getState().length).toBe(1);
+  });
+
+  it('Should update the object when using targeting', ()=>{
+    store2.dispatch(update({bar: 'bop'}));
+    expect(store2.getState().foo.bar).toBe('bop');
   });
 });
